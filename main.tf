42fffb094d5bdfa9d508d4d8d28c845c406ad114
mpanicker <m_panicker@yahoo.com>
2025-12-31 08:50:28 -0600

    added variable for db name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_cosmosdb_account" "example" {
-    name                = "cosmos-db-account-example"
+    name                = "${var.cosmosdb_name}"
     location            = azurerm_resource_group.example.location
     resource_group_name = azurerm_resource_group.example.name
     offer_type          = "Standard"
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "cosmosdb_name" {
+    description = "Name of the Cosmos DB account"
+    type        = string
+}
